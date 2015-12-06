1a2ee945ffe5862da445dcf147d70fb653f479d2
countdyang <countdyang@hotmail.com>
2015-12-06 14:54:35 -0600

    webcam

---
+++ Ch7/webcam/xbeeChat.js
@@ -0,0 +1,14 @@
+var SerialPort = require("serialport");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+
